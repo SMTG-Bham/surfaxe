c25efc109005c644b4d8383db280bb3392e93f7b
brlec <43988695+brlec@users.noreply.github.com>
2020-10-05 12:57:42 +0100

    added is_symmetric option to generation

--- surfaxe/generation.py
+++ surfaxe/generation.py
@@ -14,9 +14,9 @@ def custom_formatwarning(message, category, filename, lineno, line=''):
 PBEsol_slab_config = {
   'INCAR': {'ALGO': 'Normal', 'ADDGRID': False, 'LASPH': True, 'EDIFFG': -0.01,
             'EDIFF': 1e-06, 'ENCUT': 500, 'ISIF': 2, 'ISMEAR': 0, 'GGA': 'PS',
-            'LASPH': True, 'LREAL': 'auto', 'LORBIT': 11, 'LCHARG': False,
-            'LWAVE': False, 'NELM': 150, 'NSW': 0, 'PREC': 'Accurate',
-            'SIGMA': 0.01, 'ISYM': 2, 'IWAVPR': 1},
+            'LREAL': 'auto', 'LORBIT': 11, 'LCHARG': False, 'LWAVE': False, 
+            'NELM': 150, 'NSW': 0, 'PREC': 'Accurate', 'SIGMA': 0.01, 'ISYM': 2, 
+            'IWAVPR': 1},
   'KPOINTS': {'reciprocal_density': 90},
   'POTCAR': {'Ac': 'Ac', 'Ag': 'Ag', 'Al': 'Al', 'Ar': 'Ar', 'As': 'As',
              'Au':  'Au', 'B': 'B', 'Ba': 'Ba_sv', 'Be': 'Be_sv', 'Bi': 'Bi',
@@ -40,7 +40,7 @@ PBEsol_slab_config = {
 
 def get_one_hkl_slabs(structure, hkl, thicknesses, vacuums, make_fols=False,
                       make_input_files=False, max_size=500, lll_reduce=True,
-                      center_slab=True, ox_states=None,
+                      center_slab=True, ox_states=None, is_symmetric=True,
                       config_dict=PBEsol_slab_config, potcar_functional='PBE',
                       update_incar=None, update_kpoints=None,
                       update_potcar=None, **kwargs):
@@ -65,6 +65,9 @@ def get_one_hkl_slabs(structure, hkl, thicknesses, vacuums, make_fols=False,
         ox_states (list or dict): add oxidation states either by sites
         i.e. [3, 2, 2, 1, -2, -2, -2, -2] or by element i.e. {'Fe': 3, 'O':-2};
         default=None which adds oxidation states by guess
+        is_symmetric (bool): whether or not the slabs cleaved should have 
+        inversion symmetry. Needs to be False for slabs cleaved from a
+        non-centrosymmetric bulk; default=True
         lll_reduce (bool): whether or not the slabs will be orthogonalized;
         default=True.
         center_slab (bool): position of the slab in the unit cell, if True the
@@ -88,8 +91,9 @@ def get_one_hkl_slabs(structure, hkl, thicknesses, vacuums, make_fols=False,
     struc = Structure.from_file(structure)
     bulk_name = struc.formula.replace(" ", "")
 
-    # Adds oxidation states by guess by default or if the provided oxidation states are
-    # antyhing but a list or a dict; max_sites speeds up the by_guess method
+    # Adds oxidation states by guess by default or if the provided oxidation 
+    # states are antyhing but a list or a dict; max_sites speeds up the 
+    # by_guess method
     if type(ox_states) is dict:
         struc.add_oxidation_state_by_element(ox_states)
     elif type(ox_states) is list:
@@ -97,23 +101,33 @@ def get_one_hkl_slabs(structure, hkl, thicknesses, vacuums, make_fols=False,
     else:
         struc.add_oxidation_state_by_guess(max_sites=-1)
 
-    # Iterate through vacuums and thicknessses to get all zero dipole symmetric
-    # slabs
+    # Iterate through vacuums and thicknessses 
     provisional = []
     for vacuum in vacuums:
         for thickness in thicknesses:
             slabgen = SlabGenerator(struc, hkl, thickness, vacuum,
                                     lll_reduce=lll_reduce,
-                                    center_slab=center_slab)
+                                    center_slab=center_slab, **kwargs)
             slabs = slabgen.get_slabs()
             for i, slab in enumerate(slabs):
-                if (slab.is_polar() == False) and (slab.is_symmetric() == True):
-                    provisional.append({'hkl': ''.join(map(str, slab.miller_index)),
-                                        'slab_t': thickness,
-                                        'vac_t': vacuum,
-                                        's_index': i,
-                                        'slab': slab})
-
+                # Get all the zero-dipole slabs with inversion symmetry
+                if is_symmetric: 
+                    if slab.is_symmetric() and not slab.is_polar():
+                        provisional.append({'hkl': ''.join(map(str, slab.miller_index)),
+                                            'slab_t': thickness,
+                                            'vac_t': vacuum,
+                                            's_index': i,
+                                            'slab': slab})
+                
+                # Get all the zero-dipole slabs wihtout inversion symmetry
+                else: 
+                    if not slab.is_polar():
+                        provisional.append({'hkl': ''.join(map(str, slab.miller_index)),
+                                            'slab_t': thickness,
+                                            'vac_t': vacuum,
+                                            's_index': i,
+                                            'slab': slab})
+                  
     # Iterate though provisional slabs to extract the unique slabs
     unique_list = []
     unique_list_of_dicts = []
@@ -147,21 +161,22 @@ def get_one_hkl_slabs(structure, hkl, thicknesses, vacuums, make_fols=False,
         warnings.warn('Some generated slabs exceed the max size specified.'
         ' Slabs that exceed the max size are: ' + ', '.join(map(str, large)))
 
-    # Makes folders hkl/slab_vac_index
-    if make_fols is True:
+    # Makes folders hkl/slab_vac_index, if only make_input_files is true the 
+    # folders will also be made automatically. 
+    if make_fols or make_input_files:
         for slab in unique_list_of_dicts:
             os.makedirs(os.path.join(os.getcwd(), r'{}/{}_{}_{}'.format(slab['hkl'],
             slab['slab_t'], slab['vac_t'], slab['s_index'])), exist_ok=True)
 
             # Makes all input files (KPOINTS, POTCAR, INCAR) based on the config
             # dictionary
-            if make_input_files is True:
+            if make_input_files:
                 vis = DictSet(structure=slab['slab'],
                               config_dict=config_dict,
                               potcar_functional=potcar_functional,
                               user_incar_settings=update_incar,
                               user_potcar_settings=update_potcar,
-                              user_kpoints_settings=update_kpoints)
+                              user_kpoints_settings=update_kpoints, **kwargs)
                 vis.write_input(r'{}/{}_{}_{}'.format(slab['hkl'],
                                                       slab['slab_t'],
                                                       slab['vac_t'],
@@ -173,21 +188,22 @@ def get_one_hkl_slabs(structure, hkl, thicknesses, vacuums, make_fols=False,
                 filename=r'{}/{}_{}_{}/POSCAR'.format(slab['hkl'],
                 slab['slab_t'], slab['vac_t'], slab['s_index']))
 
-    # Makes POSCAR_hkl_slab_vac_index files in the root folder
+    # Makes POSCAR_hkl_slab_vac_index files in the bulk_name folder
     else:
         os.makedirs(os.path.join(os.getcwd(), r'{}'.format(bulk_name)),
         exist_ok=True)
-        for slab in unique_slabs_list_of_dicts:
+        for slab in unique_list_of_dicts:
             slab['slab'].to(fmt='poscar',
             filename=r'{}/POSCAR_{}_{}_{}_{}.vasp'.format(bulk_name,slab['hkl'],
             slab['slab_t'], slab['vac_t'], slab['s_index']))
 
 
 def get_all_slabs(structure, max_index, thicknesses, vacuums, make_fols=False,
-                  make_input_files=False, max_size=500, ox_states=None,
-                  lll_reduce=True, center_slab=True, config_dict=PBEsol_slab_config,
-                  potcar_functional='PBE', update_incar=None, update_potcar=None,
-                  update_kpoints=None, **kwargs):
+                  make_input_files=False, max_size=500, ox_states=None, 
+                  is_symmetric=True, lll_reduce=True, center_slab=True, 
+                  config_dict=PBEsol_slab_config, potcar_functional='PBE', 
+                  update_incar=None, update_potcar=None, update_kpoints=None, 
+                  **kwargs):
     """
     Generates all unique slabs with specified maximum Miller index, minimum slab
     and vacuum thicknesses. It includes all combinations for multiple zero
@@ -203,12 +219,16 @@ def get_all_slabs(structure, max_index, thicknesses, vacuums, make_fols=False,
         vacuums (list): minimum size of the vacuum in angstroms.
         make_fols (bool): makes folders containing POSCARs; default=False
         make_input_files (bool): makes INCAR, POTCAR and KPOINTS files in each
-        of the folders; default=False.
+        of the folders; if True but make_fols is False it will make the folders 
+        regardless; default=False.
         max_size (int): the maximum number of atoms in the slab for the size
         warning; default=500.
         ox_states (list or dict): add oxidation states either by sites
         i.e. [3, 2, 2, 1, -2, -2, -2, -2] or by element i.e. {'Fe': 3, 'O':-2};
         default=None which adds oxidation states by guess
+        is_symmetric (bool): whether or not the slabs cleaved should have 
+        inversion symmetry. Needs to be False for slabs cleaved from a
+        non-centrosymmetric bulk; default=True
         lll_reduce (bool): whether or not the slabs will be orthogonalized;
         default=True.
         center_slab (bool): position of the slab in the unit cell, if True the
@@ -234,8 +254,9 @@ def get_all_slabs(structure, max_index, thicknesses, vacuums, make_fols=False,
     struc = Structure.from_file(structure)
     bulk_name = struc.formula.replace(" ", "")
 
-    # Adds oxidation states by guess by default or if the provided oxidation states are
-    # antyhing but a list or a dict; max_sites speeds up the by_guess method
+    # Adds oxidation states by guess by default or if the provided oxidation 
+    # states are antyhing but a list or a dict; max_sites speeds up the 
+    # by_guess method
     if type(ox_states) is dict:
         struc.add_oxidation_state_by_element(ox_states)
     elif type(ox_states) is list:
@@ -243,23 +264,35 @@ def get_all_slabs(structure, max_index, thicknesses, vacuums, make_fols=False,
     else:
         struc.add_oxidation_state_by_guess(max_sites=-1)
 
-    # Iterate through vacuums and thicknessses to get all zero dipole symmetric
-    # slabs
+   
+    # Iterate through vacuums and thicknessses
     provisional = []
     for vacuum in vacuums:
         for thickness in thicknesses:
             all_slabs = generate_all_slabs(struc, max_index=max_index,
-                                           min_slab_size=thickness,
-                                           min_vacuum_size=vacuum,
-                                           lll_reduce=lll_reduce,
-                                           center_slab=center_slab)
+                                        min_slab_size=thickness,
+                                        min_vacuum_size=vacuum,
+                                        lll_reduce=lll_reduce,
+                                        center_slab=center_slab, **kwargs)
+            
             for i, slab in enumerate(all_slabs):
-                if (slab.is_polar() == False) and (slab.is_symmetric() == True):
-                    provisional.append({'hkl': ''.join(map(str, slab.miller_index)),
+                # Get all the zero-dipole slabs with inversion symmetry
+                if is_symmetric: 
+                    if (slab.is_polar() == False) and (slab.is_symmetric() == True):
+                        provisional.append({'hkl': ''.join(map(str, slab.miller_index)),
                                         'slab_t': thickness,
                                         'vac_t': vacuum,
                                         's_index': i,
                                         'slab': slab})
+                
+                # Get all the zero-dipole slabs wihtout inversion symmetry 
+                else: 
+                    if not slab.is_polar():
+                        provisional.append({'hkl': ''.join(map(str, slab.miller_index)),
+                                            'slab_t': thickness,
+                                            'vac_t': vacuum,
+                                            's_index': i,
+                                            'slab': slab})
 
     # Iterate though provisional slabs to extract the unique slabs
     unique_list = []
@@ -298,8 +331,9 @@ def get_all_slabs(structure, max_index, thicknesses, vacuums, make_fols=False,
             warnings.warn('Some generated slabs exceed the max size specified.'
             ' Slabs that exceed the max size are: ' + ', '.join(map(str, large)))
 
-    # Makes folders hkl/slab_vac_index
-    if make_fols is True:
+    # Makes folders hkl/slab_vac_index, if only make_input_files is true the 
+    # folders will also be made automatically.
+    if make_fols or make_input_files:
         for slab in unique_list_of_dicts:
             os.makedirs(os.path.join(os.getcwd(), r'{}/{}_{}_{}'.format(slab['hkl'],
             slab['slab_t'], slab['vac_t'], slab['s_index'])), exist_ok=True)
@@ -312,16 +346,17 @@ def get_all_slabs(structure, max_index, thicknesses, vacuums, make_fols=False,
                               potcar_functional=potcar_functional,
                               user_incar_settings=update_incar,
                               user_potcar_settings=update_potcar,
-                              user_kpoints_settings=update_kpoints)
+                              user_kpoints_settings=update_kpoints, **kwargs)
                 vis.write_input(os.path.join(os.getcwd(), r'{}/{}_{}_{}'.format(slab['hkl'],
                 slab['slab_t'], slab['vac_t'], slab['s_index'])))
+            
             # Makes the folders with POSCARs
             else:
                 slab['slab'].to(fmt='poscar',
                 filename=r'{}/{}_{}_{}/POSCAR'.format(slab['hkl'],slab['slab_t'],
                 slab['vac_t'], slab['s_index']))
 
-    # Omits folders, makes POSCAR_hkl_slab_vac_index files in the root folder
+    # Omits folders, makes POSCAR_hkl_slab_vac_index files in folder bulk_name
     else:
         os.makedirs(os.path.join(os.getcwd(), r'{}'.format(bulk_name)),
         exist_ok=True)
