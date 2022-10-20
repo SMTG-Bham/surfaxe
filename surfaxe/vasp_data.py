b616286bf28baece2b1870684d27a3cd4a52a0b7
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2022-10-20 13:12:25 +0900

    Parse OUTCAR or OUTCAR.gz, and vasprun.xml or vasprun.xml.gz (to save space when doing many big convergence calcs)

--- surfaxe/vasp_data.py
+++ surfaxe/vasp_data.py
@@ -115,11 +115,15 @@ save_csv=True, csv_fname='data.csv', **kwargs):
     # parse them for data
     df_list, electrostatic_list, core_energy_list = ([] for i in range(3))
 
-    for hkl_tuple, path in hkl_dict.items(): 
+    for hkl_tuple, path in hkl_dict.items():
         vsp_path = '{}/vasprun.xml'.format(path)
+        if os.path.exists(vsp_path):
+            vsp = Vasprun(vsp_path, parse_potcar_file=False)
+        elif os.path.exists(vsp_path + '.gz'):
+            vsp = Vasprun(vsp_path + '.gz', parse_potcar_file=False)
+
         psc_path = '{}/POSCAR'.format(path)
-        vsp = Vasprun(vsp_path, parse_potcar_file=False)
-        slab = slab_from_file(psc_path,hkl_tuple)
+        slab = slab_from_file(psc_path, hkl_tuple)
         vsp_dict = vsp.as_dict()
         
         df_list.append({
@@ -309,8 +313,12 @@ nn_method=CrystalNN(), outcar='OUTCAR', structure='POSCAR'):
     if type(atom) is np.float64: 
         core_energy = np.nan 
     else:       
-        # Read OUTCAR, get the core state energy 
-        otc = Outcar(outcar)
+        # Read OUTCAR, get the core state energy
+        if os.path.exists(outcar):
+            otc = Outcar(outcar)
+        elif os.path.exists(outcar + '.gz'):
+            otc = Outcar(outcar + '.gz')
+
         core_energy_dict = otc.read_core_state_eigen()
         try: 
             core_energy = core_energy_dict[atom][orbital][-1]
